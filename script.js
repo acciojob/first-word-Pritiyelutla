b939a297a9c63856935424294cd3cf9cf6d8d942
Pritiyelutla <143998125+Pritiyelutla@users.noreply.github.com>
2024-06-05 20:47:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function firstWord(s) {
-  // your code here
-}
+    // your code here
+    let str = s.trim();
+    let ans = "";
+
+    for(let i=0; i<str.length; i++)
+    {
+        if(str[i] != " ")
+        {
+            ans = ans+""+str[i];
+        }
+        else{
+            break;
+        }
+    }
+    return ans;
+  }
 
 // Do not change the code below
 
